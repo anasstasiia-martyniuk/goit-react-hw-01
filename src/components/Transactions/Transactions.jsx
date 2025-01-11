f75c0a526d019935d1dec0629f1e4473a5bd196e
anasstasiia-martyniuk <anass.tttazja@gmail.com>
2025-01-12 00:49:47 +0100

    fixed classes

--- src/components/Transactions/Transactions.jsx
+++ src/components/Transactions/Transactions.jsx
@@ -2,21 +2,21 @@ import css from "./Transactions.module.css"
 export const Transactions = ({ items }) => {
     return (
         <>
-            <table>
+            <table className={css.table}>
                 <thead>
-                    <tr>
-                        <th>Type</th>
-                        <th>Amount</th>
-                        <th>Currency</th>
+                    <tr className="css.tr">
+                        <th className="css.th">Type</th>
+                        <th className="css.th">Amount</th>
+                        <th className="css.th">Currency</th>
                     </tr>
                 </thead>
 
                 <tbody>
                     {items.map((item) => (
-                        <tr key={item.id}>
-                            <td>{item.type}</td>
-                            <td>{item.amount}</td>
-                            <td>{item.currency}</td>
+                        <tr  className="css.tr" key={item.id}>
+                            <td className="css.td">{item.type}</td>
+                            <td className="css.td">{item.amount}</td>
+                            <td className="css.td">{item.currency}</td>
                         </tr>
                     ))}
                 </tbody>
