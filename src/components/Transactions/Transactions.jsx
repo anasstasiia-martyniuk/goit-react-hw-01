790ef61b0c9f2f8c6556c01d15f066db207a2957
anasstasiia-martyniuk <anass.tttazja@gmail.com>
2025-01-12 00:56:13 +0100

    aaaaa

--- src/components/Transactions/Transactions.jsx
+++ src/components/Transactions/Transactions.jsx
@@ -2,21 +2,21 @@ import css from "./Transactions.module.css"
 export const Transactions = ({ items }) => {
     return (
         <>
-            <table className={css.table}>
+            <table className={css.list}>
                 <thead>
-                    <tr className="css.tr">
-                        <th className="css.th">Type</th>
-                        <th className="css.th">Amount</th>
-                        <th className="css.th">Currency</th>
+                    <tr className="css.tr-list">
+                        <th className="css.thItem">Type</th>
+                        <th className="css.thItem">Amount</th>
+                        <th className="css.thItem">Currency</th>
                     </tr>
                 </thead>
 
                 <tbody>
                     {items.map((item) => (
-                        <tr  className="css.tr" key={item.id}>
-                            <td className="css.td">{item.type}</td>
-                            <td className="css.td">{item.amount}</td>
-                            <td className="css.td">{item.currency}</td>
+                        <tr  className="css.trList" key={item.id}>
+                            <td className="css.tdItem">{item.type}</td>
+                            <td className="css.tdItem">{item.amount}</td>
+                            <td className="css.tdItem">{item.currency}</td>
                         </tr>
                     ))}
                 </tbody>
